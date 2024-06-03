fb09cc54490538aaf6f1043edd08b931bee1ea64
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-03 20:09:05 +0600

    created get api for property

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbuydyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,6 +46,13 @@ async function run() {
     app.get('/property', async(req, res) => {
         const result = await propertyCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/property/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await propertyCollection.findOne(query)
+        res.send(result)
     })
 
 
