29b6bca8becd496357a287e5df8239dd8cdf5545
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-05 01:01:13 +0600

    created delete api for wishlist

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/wishlist/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await wishlistCollection.deleteOne(query)
+        res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
