a1a9815e5537567849c650b30b4324a4d1b8dd93
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-05 21:58:43 +0600

    verify admin or agent

--- index.js
+++ index.js
@@ -45,22 +45,81 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({message: 'forbidden access'})
+        return res.status(401).send({message: 'unauthorized access'})
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({message: 'forbidden access'})
+          return res.status(401).send({message: 'unauthorized access'})
         }
         req.decoded = decoded;
         next();
       })
     }
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
+
+    const verifyAgent = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAgent = user?.role === 'agent';
+      if (!isAgent) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
+
     // ----- Users API -----
-    app.get('/user', verifyToken, async(req, res) => {
+    app.get('/user', verifyToken, verifyAdmin, async(req, res) => {
         const result = await userCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+    })
+
+    app.get('/user/normal/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      res.send(user)
+    })
+
+    app.get('/user/agent/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      let agent = false;
+      if (user) {
+        agent = user?.role === 'agent'
+      }
+      res.send({agent})
     })
 
     app.post('/user', async(req, res) => {
