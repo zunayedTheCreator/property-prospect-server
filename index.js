7b54579d2986eae036df0e0bde572d50c05eb664
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-05 19:04:11 +0600

    created patch api to make agent for user

--- index.js
+++ index.js
@@ -62,6 +62,18 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/user/agent/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'agent'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
     app.delete('/user/:id', async(req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
