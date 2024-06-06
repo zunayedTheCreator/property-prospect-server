64b1a1cd4fc984221cbe6b1afeca1d4fd5333a7a
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-06-06 19:46:36 +0600

    created delete api for property

--- index.js
+++ index.js
@@ -215,6 +215,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/property/:id',verifyToken, verifyAgent, async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await propertyCollection.deleteOne(query)
+        res.send(result)
+    })
+
     // ----- WishLists API -----
     app.get('/wishlist', async(req, res) => {
         const result = await wishlistCollection.find().toArray();
